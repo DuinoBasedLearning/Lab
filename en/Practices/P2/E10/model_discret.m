bfaeaff89d7c1a10c63c9e9196b593887d2ba2f0
DuinoBasedLearning <42932730+DuinoBasedLearning@users.noreply.github.com>
2018-12-03 12:52:42 +0100

    Delete model_discret.m

--- en/Practices/P2/E10/model_discret.m
+++
@@ -1,9 +0,0 @@
-% Arxiu corresponent a la modelitzacio del sistema
-function [Y]=model_discret(y0,K0,Kp,Kt,tau0,Ts,X)
-    i=2;
-    [fils cols]=size(X);
-    Y=[y0];
-    while (i<=cols)
-        Y=[Y X(1,i-1)*Kp*K0*(1-exp(-Ts/tau0))-(Kt*Kp*K0*(1-exp(-Ts/tau0))-exp(-Ts/tau0))*Y(1,i-1)];
-        i=i+1;
-    end
